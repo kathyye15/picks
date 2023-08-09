cd0803bd936ea2153827b17b1c6845851292271b
Kathy Ye <kathyye15@gmail.com>
2023-08-08 22:55:55 -0700

    render google map

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import Head from 'next/head';
+import Head from "next/head";
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 
 export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+  if (!isLoaded) return <h1>loading........</h1>;
   return (
     <>
       <Head>
         <title>picks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Hello Picks!</div>
+      <Map />
     </>
-  )
+  );
+}
+
+function Map() {
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <MarkerF position={center} />
+    </GoogleMap>
+  );
 }
