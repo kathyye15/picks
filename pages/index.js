d038e311cb843df3e88a35f778e97f09b3612a48
Kathy Ye <kathyye15@gmail.com>
2023-08-08 20:24:35 -0700

    nextjs setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>picks</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Hello Picks!</div>
+    </>
+  )
+}
