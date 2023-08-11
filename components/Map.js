8c8a1d7f8738637b73b36aa0a57af9834e15f58e
Kathy Ye <kathyye15@gmail.com>
2023-08-10 23:54:46 -0700

    Add autocomplete address search bar
    Implement feature where users can search for addresses and select from suggestions provided by the autocomplete functionality. Map is dynamically rendered upon select.

--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,81 @@
-import { useMemo } from "react";
+import React, { useState } from "react";
 import { GoogleMap, MarkerF } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import { OverlayTrigger, Popover, ListGroup } from "react-bootstrap";
 
 export default function Map() {
-  const center = useMemo(
-    () => ({ lat: 37.74330863129275, lng: -122.477352087827 }),
-    []
-  );
+  const [selected, setSelected] = useState(null);
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <MarkerF position={center} />
-    </GoogleMap>
+    <>
+      <div className="places-container">
+        <PlacesAutocomplete setSelected={setSelected} />
+      </div>
+      <GoogleMap
+        zoom={10}
+        center={selected}
+        mapContainerClassName="map-container"
+      >
+        {selected && <MarkerF position={selected} />}
+      </GoogleMap>
+    </>
+  );
+}
+
+function PlacesAutocomplete({ setSelected }) {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
+  };
+
+  const popover = (
+    <Popover id="popover-search-results">
+      <Popover.Body>
+        <ListGroup>
+          {data.map(({ place_id, description }) => (
+            <ListGroup.Item
+              key={place_id}
+              onClick={() => handleSelect(description)}
+            >
+              {description}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Popover.Body>
+    </Popover>
+  );
+
+  return (
+    <div id="searchbox">
+      <OverlayTrigger
+        trigger="click"
+        placement="bottom"
+        overlay={popover}
+        show={status === "OK" && value !== ""}
+      >
+        <input
+          className="search-input"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={!ready}
+          placeholder="Search an address"
+        />
+      </OverlayTrigger>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import { useLoadScript } from "@react-google-maps/api";
 import Map from "../components/Map";
 
+const libraries = ["places"];
 export default function Home() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: libraries,
   });
   if (!isLoaded) return <h1>loading........</h1>;
-  return <Map />;
+
+  return (
+    <>
+      <Map />
+    </>
+  );
 }
