d3a975d569b7d4de7e195c795c2b8a213181fd85
Kathy Ye <kathyye15@gmail.com>
2023-09-05 23:22:54 -0700

    Refactor: Update sidebar explore view layout and populate its contents

--- components/Sidebar/explore.js
+++ components/Sidebar/explore.js
@@ -28,35 +28,70 @@ export default function Explore() {
 
   return (
     <div className="explore-sidebar">
-      <div className="interacted">
-        <h1>{userSelectedPick?.name}</h1>
-        {userSelectedPick?.name ? (
-          <img
-            src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${userSelectedPick?.photos?.[0]?.photo_reference}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
-            alt="attraction photo"
-          />
-        ) : (
-          <div>no image</div>
-        )}
+      <h1>{userSelectedPick?.name}</h1>
+      {userSelectedPick?.name ? (
+        <img
+          src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${userSelectedPick?.photos?.[0]?.photo_reference}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
+          alt="attraction photo"
+        />
+      ) : (
+        <div>no image</div>
+      )}
 
-        <button onClick={toggleSaveCallback}>{saveButtonText}</button>
-        <button
-          onClick={() => {
-            setInExploreView(!inExploreView);
-          }}
-        >
-          toggle explore/saved view
-        </button>
+      <button onClick={toggleSaveCallback}>{saveButtonText}</button>
+      <button
+        onClick={() => {
+          setInExploreView(!inExploreView);
+        }}
+      >
+        toggle explore/saved view
+      </button>
 
-        <div>description: </div>
-        <ul>
-          <li>{`ratings & reviews: ${userSelectedPick?.rating} stars, ${userSelectedPick?.user_ratings_total} reviews`}</li>
-          <li>price: </li>
-          <li>{`address: ${userSelectedPick?.formatted_address}`}</li>
-          <li>hours: </li>
-          <li>parking info: </li>
-        </ul>
-      </div>
+      <ul>
+        <li>{`Ratings & Reviews: ${userSelectedPick?.rating} stars, ${userSelectedPick?.user_ratings_total} reviews`}</li>
+        <li>
+          Price:{" "}
+          {Number.isInteger(userSelectedPick?.price_level)
+            ? userSelectedPick.price_level === 0
+              ? "Free"
+              : "$".repeat(userSelectedPick.price_level)
+            : "Not available"}
+        </li>
+        <li>
+          Address: {userSelectedPick?.formatted_address || "Not available"}
+        </li>
+        <li>
+          Hours:
+          {userSelectedPick?.current_opening_hours?.weekday_text?.length ? (
+            <ul>
+              {userSelectedPick.current_opening_hours.weekday_text.map(
+                (daySchedule, index) => (
+                  <li key={index}>{daySchedule}</li>
+                )
+              )}
+            </ul>
+          ) : (
+            "Not available"
+          )}
+        </li>
+        <li>
+          Phone: {userSelectedPick?.formatted_phone_number || "Not available"}
+        </li>
+        <li>
+          Website:
+          {userSelectedPick?.website ? (
+            <a
+              href={userSelectedPick.website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {userSelectedPick.website}
+            </a>
+          ) : (
+            "Not available"
+          )}
+        </li>
+      </ul>
     </div>
   );
 }
