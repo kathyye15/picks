87d4fadf1c25dbb6d3df1cb176300034f3fc829e
Kathy Ye <kathyye15@gmail.com>
2023-09-08 15:57:32 -0700

    Introduce 'userSelectedPickID' context for dynamic API key endpoints in SWR

--- contexts/AppContext.js
+++ contexts/AppContext.js
@@ -9,6 +9,7 @@ export default function AppContextProvider({ children }) {
     useState(null);
   const [searchedCity, setSearchedCity] = useState("");
   const [userSelectedPick, setUserSelectedPick] = useState({});
+  const [userSelectedPickID, setUserSelectedPickID] = useState("");
   const [nearbyPicks, setNearbyPicks] = useState([]);
   const [savedPicks, setSavedPicks] = useState([]);
   const [directionsResponse, setDirectionsResponse] = useState(null);
@@ -26,6 +27,8 @@ export default function AppContextProvider({ children }) {
         setSearchedCity,
         userSelectedPick,
         setUserSelectedPick,
+        userSelectedPickID,
+        setUserSelectedPickID,
         nearbyPicks,
         setNearbyPicks,
         savedPicks,
