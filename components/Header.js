c3830ce841d478e9431d8368ec98dfcbbde6ab03
Kathy Ye <kathyye15@gmail.com>
2023-09-07 19:06:43 -0700

    Customize Header and Picks icon with brand colors; Adjust Header layout

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Image from "next/image";
+import { Flex, Box, HStack, Text } from "@chakra-ui/react";
 
 export default function Header() {
   return (
-    <header>
-      <Image
-        src="/svg/picksIcon.svg"
-        alt="picks icon"
-        width={100}
-        height={100}
-      />
-      <span>Picks</span>
-    </header>
+    <Flex bg="brand.cadetGray" minWidth="max-content">
+      <HStack>
+        <Box p="2">
+          <Image
+            src="/svg/picksIcon.svg"
+            alt="picks icon"
+            width={50}
+            height={50}
+          />
+        </Box>
+        <Box>
+          <Text fontSize="2xl" color="brand.navy" mt={7}>
+            Picks
+          </Text>
+        </Box>
+      </HStack>
+    </Flex>
   );
 }
