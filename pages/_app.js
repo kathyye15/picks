169f3dcb0a6fc6df3a66aab92d0c9477a70bf539
Kathy Ye <kathyye15@gmail.com>
2023-09-07 18:58:49 -0700

    Configure ChakraProvider, customize theme with brand colors, and set default font to Noto Sans

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,34 @@
 import Layout from "../components/layout";
-import React from "react";
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { Noto_Sans } from "next/font/google";
+
+const noto_sans = Noto_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const colors = {
+  brand: {
+    navy: "#33658A",
+    slateGray: "#7A8B99",
+    cadetGray: "#91ADC2",
+    coolGray: "#9BA0BC",
+    thistle: "#C1B8C8",
+  },
+};
+
+export const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={noto_sans.className}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </main>
   );
 }
