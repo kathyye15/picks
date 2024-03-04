cbfa230aff7134093634d52678edc26c3f89cd26
Kathy Ye <kathyye15@gmail.com>
2024-03-04 00:36:09 -0800

    create login and sign-up ui components

--- components/Auth/LoginAndSignUp.js
+++ components/Auth/LoginAndSignUp.js
@@ -1,14 +1,41 @@
-import { HStack, Button } from '@chakra-ui/react';
+import { useContext } from 'react';
+import { HStack, Button, useDisclosure } from '@chakra-ui/react';
+import SignUpModal from './SignUpModal';
+import LoginModal from './LoginModal';
+import { AppContext } from '../../contexts/AppContext';
 
 export default function LoginAndSignUp() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { modalType, setModalType } = useContext(AppContext);
+
+  const openModal = (type) => {
+    setModalType(type);
+    onOpen();
+  };
   return (
-    <HStack spacing="2" marginRight={4}>
-      <Button variant="outline" color="brand.navy">
-        Login
-      </Button>
-      <Button variant="outline" color="brand.navy">
-        Sign Up
-      </Button>
-    </HStack>
+    <>
+      <HStack spacing="2" marginRight={4}>
+        <Button
+          variant="outline"
+          color="brand.navy"
+          onClick={() => openModal('login')}
+        >
+          Login
+        </Button>
+        <Button
+          variant="outline"
+          color="brand.navy"
+          onClick={() => openModal('signup')}
+        >
+          Sign Up
+        </Button>
+      </HStack>
+      {modalType === 'login' && (
+        <LoginModal isOpen={isOpen} onClose={onClose} />
+      )}
+      {modalType === 'signup' && (
+        <SignUpModal isOpen={isOpen} onClose={onClose} />
+      )}
+    </>
   );
 }
