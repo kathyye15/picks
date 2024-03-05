235889c702d6756f00a6064be849953235917597
Kathy Ye <kathyye15@gmail.com>
2024-03-05 02:57:41 -0800

    New Functionality: Implement Login Authentication
    -add firebase service functionality
    -replace Chakra UI form control due to not having ready-to-use form state and validation

--- components/Auth/LoginModal.js
+++ components/Auth/LoginModal.js
@@ -7,36 +7,53 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
-  FormControl,
   FormLabel,
   Input,
 } from '@chakra-ui/react';
+import { auth } from '../../firebase-config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import formatErrorCodeToMessage from '../../utils/formatErrorCodeToMessage';
+
+const loginEmailPassword = async (event) => {
+  event.preventDefault();
+  const loginEmail = event.target.email.value;
+  const loginPassword = event.target.password.value;
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      loginEmail,
+      loginPassword
+    );
+    console.log(userCredential);
+  } catch (error) {
+    let errorMessage = formatErrorCodeToMessage(error.code);
+    console.log(errorMessage);
+  }
+};
 
 export default function LoginModal({ isOpen, onClose }) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Log in to your account</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody pb={6}>
-          <FormControl isRequired mt={3}>
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" placeholder="Email address" />
-          </FormControl>
-          <FormControl isRequired mt={3}>
-            <FormLabel>Password</FormLabel>
-            <Input type="password" placeholder="Password" />
-          </FormControl>
-        </ModalBody>
+      <form onSubmit={loginEmailPassword}>
+        <ModalContent>
+          <ModalHeader>Log in to your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input id="email" type="email" placeholder="Email address" />
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input id="password" type="password" placeholder="Password" />
+          </ModalBody>
 
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3}>
-            Sign In
-          </Button>
-          <Button onClick={onClose}>Cancel</Button>
-        </ModalFooter>
-      </ModalContent>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} type="submit">
+              Sign In
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
     </Modal>
   );
 }
